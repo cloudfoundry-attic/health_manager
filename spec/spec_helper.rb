29e848247e041d64e8930fe91ec4028aff0d825f
Bob Nugmanov <bnugmanov@vmware.com>
2012-06-12 16:33:59 -0700

    Reorg specs
    Change-Id: Ic5ef366dc6e0581810a7ff0d257cb4700d2ac604

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,17 @@
 # Copyright (c) 2009-2012 VMware, Inc.
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-home = File.join(File.dirname(__FILE__), '..')
-ENV['BUNDLE_GEMFILE'] = "#{home}/Gemfile"
-
 require 'rubygems'
 require 'rspec'
 require 'bundler/setup'
 
 require 'health_manager'
+
+support_dir = File.join(File.dirname(__FILE__),"support")
+Dir["#{support_dir}/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+ config.include(HealthManager)
+end
+
 
