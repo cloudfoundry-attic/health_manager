0eaa71dcd9a4a89faa4fbdc51cba673cfd87fba2
Bob Nugmanov and Maria Shaldibina <mariash@vmware.com>
2013-02-12 16:45:30 -0800

    bulk API retry logic
    Change-Id: Ifb48a8cb13e8a0328b39f5ffbba6ed49f0ab56a5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.include(HealthManager::Common)
 
   config.before :all do
-    logging_config = {'level' => ENV['LOG_LEVEL'] || 'warn'}
+    logging_config = { 'level' => ENV['LOG_LEVEL'] || 'fatal' }
     steno_config = Steno::Config.to_config_hash(logging_config)
     steno_config[:context] = Steno::Context::ThreadLocal.new
     Steno.init(Steno::Config.new(steno_config))
