db63766c9ba994909da28d85758a41bfaca823c9
Jon Stevens and Maria Shaldibina <pair+jon+mariash@pivotallabs.com>
2013-05-02 17:59:57 -0700

    Test all harmonizer listeners

--- spec/unit/harmonizer_spec.rb
+++ spec/unit/harmonizer_spec.rb
@@ -4,6 +4,7 @@ module HealthManager
   describe Harmonizer do
     let(:nudger) { mock.as_null_object }
     let(:expected_state_provider) { mock.as_null_object }
+    let(:known_state_provider) { mock.as_null_object }
     let(:scheduler) { mock.as_null_object }
     let(:varz) { mock.as_null_object }
     let(:app) do
@@ -18,17 +19,81 @@ module HealthManager
     subject do
       Harmonizer.new({
         :health_manager_component_registry => {:nudger => nudger},
-      }, varz, nudger, scheduler, nil, expected_state_provider)
+      }, varz, nudger, scheduler, known_state_provider, expected_state_provider)
     end
 
     describe "#prepare" do
-      let(:app_state) { AppState.new("app-id") }
+      let(:app_state) do
+        app_state = AppState.new("app-id")
+        app_state.stub(:get_instance) do |ind|
+          instances = [
+            {"state" => "FLAPPING"},
+            {"state" => "RUNNING"}
+          ]
+          instances[ind]
+        end
+        app_state
+      end
 
       describe "listeners" do
-        context "on droplet update" do
-          before { subject.prepare }
-          after { AppState.remove_all_listeners }
+        before { subject.prepare }
+        after { AppState.remove_all_listeners }
 
+        describe "on missing instances" do
+          context "when expected state update is required" do
+            before { app_state.expected_state_update_required = false }
+
+            context "when instance is flapping" do
+              it "executes flapping policy" do
+                subject.should_receive(:execute_flapping_policy).with(app_state, 0, {"state" => "FLAPPING"}, false)
+                AppState.notify_listener(:missing_instances, app_state, [0])
+              end
+            end
+
+            context "when instance is NOT flapping" do
+              it "executes NOT flapping policy" do
+                nudger.should_receive(:start_instance).with(app_state, 1, NORMAL_PRIORITY)
+                AppState.notify_listener(:missing_instances, app_state, [1])
+              end
+            end
+          end
+        end
+
+        describe "on extra_instances" do
+          context "when expected state update is required" do
+            before { app_state.expected_state_update_required = false }
+
+            it "stops instances immediately" do
+              nudger.should_receive(:stop_instances_immediately).with(app_state, [1, 2])
+              AppState.notify_listener(:extra_instances, app_state, [1, 2])
+            end
+          end
+        end
+
+        describe "on exit dea" do
+          it "starts instance with high priority" do
+            nudger.should_receive(:start_instance).with(app_state, 5, HIGH_PRIORITY)
+            AppState.notify_listener(:exit_dea, app_state, {"index" => 5})
+          end
+        end
+
+        describe "on exit_crashed" do
+          context "when instance is flapping" do
+            it "executes flapping policy" do
+              subject.should_receive(:execute_flapping_policy).with(app_state, 0, {"state" => "FLAPPING"}, true)
+              AppState.notify_listener(:exit_crashed, app_state, {"version" => 0, "index" => 0})
+            end
+          end
+
+          context "when instance is NOT flapping" do
+            it "executes NOT flapping policy" do
+              nudger.should_receive(:start_instance).with(app_state, 1, LOW_PRIORITY)
+              AppState.notify_listener(:exit_crashed, app_state, {"version" => 1, "index" => 1})
+            end
+          end
+        end
+
+        describe "on droplet update" do
           def test_listener
             AppState.notify_listener(:droplet_updated, app_state)
           end
